50e6ff3f3e903dfe809d75c3d2d88ffe5d4e1966
yihuawei <wwyihua@gmail.com>
2022-04-06 00:44:49 -0500

    add pattern

--- graph_converter/prepare_data.sh
+++ graph_converter/prepare_data.sh
@@ -6,9 +6,26 @@ graphs=(
     https://snap.stanford.edu/data/bigdata/communities/com-lj.ungraph.txt.gz
 )
 
+txtdir=../data/txt_graph/
+bindir=../data/bin_graph/
+mkdir -p ${txtdir}
+mkdir -p ${bindir}
+
 for graph in ${graphs[@]}
 do
-    wget -nc -P ./data/txt_graph ${graph}
+    wget -nc -P ${txtdir} ${graph}
+done
+gzip -f -d ${txtdir}*
+
+for graph in ${txtdir}*
+do
+    filename=${graph##*/}
+    filename=${filename%.txt}
+    mkdir -p ${bindir}/${filename}
+    cp ${graph} ${bindir}/${filename}/snap.txt
 done
 
-gzip -f -d ./data/txt_graph/*
+for graph in ${bindir}*
+do
+    bash convert.sh ${graph}
+done
