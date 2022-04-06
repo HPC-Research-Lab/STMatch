e91c69567722e2c0b24a0648186e43667cde4a24
yihuawei <wwyihua@gmail.com>
2022-04-06 02:00:13 -0500

    update

--- graph_converter/prepare_data.sh
+++ graph_converter/prepare_data.sh
@@ -2,8 +2,11 @@ graphs=(
     https://snap.stanford.edu/data/wiki-Vote.txt.gz 
     https://snap.stanford.edu/data/email-Enron.txt.gz
     https://snap.stanford.edu/data/bigdata/communities/com-amazon.ungraph.txt.gz
+    https://snap.stanford.edu/data/bigdata/communities/com-dblp.ungraph.txt.gz
     https://snap.stanford.edu/data/bigdata/communities/com-youtube.ungraph.txt.gz
-    https://snap.stanford.edu/data/soc-LiveJournal1.txt.gz
+    #https://snap.stanford.edu/data/soc-LiveJournal1.txt.gz
+    #https://snap.stanford.edu/data/bigdata/communities/com-orkut.ungraph.txt.gz
+    #https://snap.stanford.edu/data/bigdata/communities/com-friendster.ungraph.txt.gz
 )
 
 txtdir=../data/txt_graph/
